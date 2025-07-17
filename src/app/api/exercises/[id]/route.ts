e88023fc1a43d5be9327eb28c0af29882a8fe5bf
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-07-17 01:16:05 -0300

    fix(routes): trocando "params" por "context"

--- src/app/api/exercises/[id]/route.ts
+++ src/app/api/exercises/[id]/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest } from 'next/server';
 import { getExercise, updateExerciseController, deleteExerciseController } from '@/server/controller/exercise';
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-  return getExercise(request, { params });
+export async function GET(request: NextRequest, context: { params: { id: string } }) {
+  return getExercise(request, context);
 }
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-  return updateExerciseController(request, { params });
+export async function PUT(request: NextRequest, context: { params: { id: string } }) {
+  return updateExerciseController(request, context);
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-  return deleteExerciseController(request, { params });
+export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
+  return deleteExerciseController(request, context);
 }
