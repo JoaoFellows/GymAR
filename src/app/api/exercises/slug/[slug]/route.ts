7e5c8217b670d33a566275b0aaa63b8ec3fa50c9
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-07-17 02:27:02 -0300

    backend deletado

--- src/app/api/exercises/slug/[slug]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from 'next/server';
-import { getExerciseBySlugController } from '@/server/controller/exercise';
-
-export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
-  return getExerciseBySlugController(request, { params });
-}
